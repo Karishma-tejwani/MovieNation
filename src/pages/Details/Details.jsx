6b29bb689b7ff89148d764f106270e0cc0cb22d0
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-04 23:44:23 +0500

    Details

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import DetailsBanner from './DetailsBanner'
-import useFetch from '../../hooks/useFetch';
+import React from "react";
+import DetailsBanner from "./DetailsBanner";
+import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
-import Cast from './Cast';
-import OfficialVideos from './OfficialVideos';
+import Cast from "./Cast";
+import OfficialVideos from "./OfficialVideos";
+import SimilarVideos from "./SimilarVideos";
+import Recommend from "./Recommend";
 
 const Details = () => {
-
-  const {mediaType, id} = useParams();
-  const {data, loading} = useFetch(`/${mediaType}/${id}/videos`);
-  const {data: credits, loading: LoadCredits} = useFetch(`/${mediaType}/${id}/credits`);
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: LoadCredits } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
 
   return (
     <div>
-      <DetailsBanner 
-        video={data?.results?.[0]} 
-        crew={credits?.crew}
-      />
-      <Cast data={credits?.cast} loading={LoadCredits}/>
-      <OfficialVideos data={data} loading={loading}/>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={LoadCredits} />
+      <OfficialVideos data={data} loading={loading} />
+      <SimilarVideos mediaType={mediaType} id={id} />
+      <Recommend mediaType={mediaType} id={id} />
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
