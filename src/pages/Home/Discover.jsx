a41fcb732d0cf16332170745020a31692d5b2f23
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-04 16:36:09 +0500

    Discover

--- src/pages/Home/Discover.jsx
+++ src/pages/Home/Discover.jsx
@@ -2,22 +2,31 @@ import React, { useState } from "react";
 import ContentWrap from "../../components/ContentWrap";
 import useFetch from "../../hooks/useFetch";
 import Carousel from "../../components/Carousel";
+import Switching from "../../components/Switching";
 
-function Trending() {
+function Discover() {
 
     const [endpoint, setEndpoint] = useState("day");
     const {data, loading} = useFetch(`/trending/movie/${endpoint}`);
+
+    // const onTabChange = (tab) => {
+    //     setEndpoint(tab === "Day" ? "day" : "week" );
+    // }
 
     return(
         <div className="carouselSection">
             <ContentWrap>
                 <span className="carouselTitle">
-                    Trending
+                    Discover
                 </span>
+                {/* <Switching 
+                    data = {["Day", "Week"]} 
+                    onTabChange = {onTabChange}
+                /> */}
             </ContentWrap>
-            <Carousel data={data?.results} loading={loading} />
+            <Carousel data={data?.results} loading={loading}/>
         </div>
     )
 }
 
-export default Trending;
+export default Discover;
