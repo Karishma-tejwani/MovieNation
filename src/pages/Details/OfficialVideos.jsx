8c2b2f5c64bf928f6861df93a084af8d1b5669e6
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-04 23:30:04 +0500

    Official Videos

--- src/pages/Details/OfficialVideos.jsx
+++ src/pages/Details/OfficialVideos.jsx
@@ -1,62 +1,69 @@
 import React, { useState } from "react";
 import ContentWrap from "../../components/ContentWrap";
-import LazyLoadImg from '../../components/lazyLoadImg';
+import LazyLoadImg from "../../components/lazyLoadImg";
 import PlayButton from "./PlayButton";
 import VideoPlay from "./VideoPlay";
+import avatar from "../../assets/avatar.png";
 
 const OfficialVideos = ({ data, loading }) => {
-    const [show, setShow] = useState(false);
-    const [videoId, setVideoId] = useState(null);
+  const [show, setShow] = useState(false);
+  const [videoId, setVideoId] = useState(null);
 
-    const loadingSkeleton = () => {
-        return (
-            <div className="skItem">
-                <div className="thumb skeleton"></div>
-                <div className="row skeleton"></div>
-                <div className="row2 skeleton"></div>
-            </div>
-        );
-    };
-
+  const loadingSkeleton = () => {
     return (
-        <div className="videosSection">
-            <ContentWrap>
-                <div className="sectionHeading">Official Videos</div>
-                {!loading ? (
-                    <div className="videos">
-                        {data?.results?.map((video) => {
-                            <div 
-                                key={video.id} 
-                                className="videoItem"
-                                onClick={() => {
-                                    setVideoId(video.key)
-                                    setShow(true)
-                                }}
-                            >
-                                <div className="videoThumbnail">
-                                    <LazyLoadImg src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`} />
-                                    <PlayButton />
-                                </div>
-                            </div>
-                        })}
-                    </div>
-                ) : (
-                    <div className="videoSkeleton">
-                        {loadingSkeleton()}
-                        {loadingSkeleton()}
-                        {loadingSkeleton()}
-                        {loadingSkeleton()}
-                    </div>
-                )}
-            </ContentWrap>
-            <VideoPlay
-                show={show}
-                setShow={setShow}
-                videoId={videoId}
-                setVideoId={setVideoId}
-            />
-        </div>
+      <div className="skItem">
+        <div className="thumb skeleton"></div>
+        <div className="row skeleton"></div>
+        <div className="row2 skeleton"></div>
+      </div>
     );
+  };
+
+  return (
+    <div className="videosSection">
+      <ContentWrap>
+        <div className="sectionHeading">Official Videos</div>
+        {!loading ? (
+          <div className="videos">
+            {data?.results?.map((video) => {
+              return (
+                <>
+                  <div
+                    key={video.id}
+                    className="videoItem"
+                    onClick={() => {
+                      setVideoId(video.key);
+                      setShow(true);
+                    }}
+                  >
+                    <div className="videoThumbnail">
+                      <LazyLoadImg
+                        src={`https://img.youtube.com/vi/${video.key}/hqdefault.jpg`}
+                      />
+                      <PlayButton />
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="videoSkeleton">
+            {loadingSkeleton()}
+            {loadingSkeleton()}
+            {loadingSkeleton()}
+            {loadingSkeleton()}
+          </div>
+        )}
+      </ContentWrap>
+      <VideoPlay
+        show={show}
+        setShow={setShow}
+        videoId={videoId}
+        setVideoId={setVideoId}
+      />
+    </div>
+  );
 };
 
 export default OfficialVideos;
