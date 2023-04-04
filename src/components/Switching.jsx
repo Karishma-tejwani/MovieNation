f9743f62a3306b07070c883f930795d156c70a34
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-04 15:58:08 +0500

    Switching Tabs

--- src/components/Switching.jsx
+++ src/components/Switching.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
 import ContentWrap from "./ContentWrap";
 
-function Switching() {
+function Switching({data, onTabChange}) {
+    const [selectedTab, setSelectedTab] = useState(0);
+    const [left, setLeft] = useState(0);
+
+    const activeTab = (tab, ind) => {
+        setLeft(ind * 100);
+        setTimeout(() => {
+            setSelectedTab(ind);
+        }, 300);
+        onTabChange(tab, ind);
+    }
+
     return(
-        <div>
-           
+        <div className="switchingTabs">
+            <div className="tabItems">
+                {data.map((tab, ind) => (
+                    <span 
+                        key={ind} 
+                        className={`tabItem ${selectedTab === ind ? "active" : ""}`}
+                        onClick={() => activeTab(tab, ind)}
+                    >
+                        {tab}
+                    </span>
+                ))}
+                <span className="movingBg" style={{left}} />
+            </div>
         </div>
     )
 }
